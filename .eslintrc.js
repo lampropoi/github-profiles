7fa96701fadf1e3c7887fc4fdbacb6bba51f9b36
Ioanna Lampropoulou <lampropoi@gmail.com>
2017-08-19 13:51:13 +0100

    eslint added

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: [
+    'airbnb'
+  ],
+  rules: {
+    'arrow-parens': [2, 'as-needed', {'requireForBlockBody': false}],
+    'comma-dangle': ['off'],
+    'jsx-a11y/img-has-alt': 0,
+    'jsx-quotes': ['error', 'prefer-single'],
+    'import/no-unresolved': [2, {ignore: ['\.png$']}],
+    'import/prefer-default-export': 'off',
+    'max-len': ['error', 120],
+    'no-inline-comments': ['error'],
+    'no-loop-func': 'off',
+    'no-mixed-operators': 'off',
+    'no-plusplus': 'off',
+    'no-return-assign': 'off',
+    'no-underscore-dangle': 'off',
+    'object-curly-spacing': ['error', 'never'],
+    'react/forbid-prop-types': ['error', {'forbid': ['array', 'object']}],
+    'react/jsx-boolean-value': ['error', 'always'],
+    'react/jsx-filename-extension': [1, {'extensions': ['.js', '.jsx']}],
+    "jsx-a11y/href-no-hash": "off",
+    "jsx-a11y/anchor-is-valid": ["warn", { "aspects": ["invalidHref"] }]
+  },
+  env: {
+    browser: true
+  }
+};
